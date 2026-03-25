7b6748d86c1e22e5303d99df3c696410be2463c9
Samuel Ozechi <ozechisamuel@gmail.com>
2026-03-25 17:47:24 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // ADD THIS LINE BELOW
-  base: "/samuel-ozechi/", 
+  base: "/", 
   server: {
     host: "::",
     port: 8080,
